0df2f1b22c504bffe749b31fae67816b2d3807c5
Sara Selami <saraselami@gmail.com>
2021-01-13 13:15:28 +0100

    message commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,16 @@
 export const environment = {
-  production: true
+  production: true,
+  
+  firebase: {
+    apiKey: "AIzaSyAQkWpY3xYJv1GQMq_Dc4qyywujhVOUmz8",
+    authDomain: "ikub-task-manager.firebaseapp.com",
+    databaseURL:'https://ikub-task-manager-default-rtdb.europe-west1.firebasedatabase.app/',
+    projectId: "ikub-task-manager",
+    storageBucket: "ikub-task-manager.appspot.com",
+    messagingSenderId: "1043073005337",
+    appId: "1:1043073005337:web:fc68f8deda88784d34b788",
+    measurementId: "G-8VL6CP6XLG"
+  }
+ 
+
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAQkWpY3xYJv1GQMq_Dc4qyywujhVOUmz8",
+    authDomain: "ikub-task-manager.firebaseapp.com",
+    databaseURL:'https://ikub-task-manager-default-rtdb.europe-west1.firebasedatabase.app/',
+    projectId: "ikub-task-manager",
+    storageBucket: "ikub-task-manager.appspot.com",
+    messagingSenderId: "1043073005337",
+    appId: "1:1043073005337:web:fc68f8deda88784d34b788",
+    measurementId: "G-8VL6CP6XLG"
+  }
 };
 
 /*
